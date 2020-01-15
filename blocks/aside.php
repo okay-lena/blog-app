f058d4b295c2f9a963abe8b8ce41dcce3002073e
lenakosyakova <kosyakova@gmail.com>
2020-01-15 15:04:09 -0500

    Changed text

--- blocks/aside.php
+++ blocks/aside.php
@@ -1,6 +1,6 @@
 <aside class="col-md-4">
   <div class="p-3 mb-3 bg-warning rounded">
     <h4><b>It's interesting</b></h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet asperiores quia sed placeat at odio, et excepturi? Doloremque odit, quidem odio facere quia architecto alias sed illo hic incidunt distinctio!</p>
+    <p>If you <u><a style="color:black" href="/login">login</a></u>, you can share your brilliant ideas and comment other posts! <br><br>Well, ok, you can comment posts anyway ))</p>
   </div>
 </aside>
