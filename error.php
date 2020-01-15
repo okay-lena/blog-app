2837767dc889984e95fd6b84c4a4117fd04dbc8b
lenakosyakova <kosyakova@gmail.com>
2020-01-15 14:46:29 -0500

    Custom error page

---
+++ error.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="icon" href="/img/favicon.ico">
+  <title>404 Not Found</title>
+</head>
+<body class="d-flex flex-column h-100" cz-shortcut-listen="true">
+  <?php require 'blocks/header.php'; ?>
+  <main role="main" class="flex-shrink-0">
+    <div class="container mt-6">
+      <p>Looks like you're lost. Please go to the <a href="/">homepage</a>.</p>
+    </div>
+  </main>
+  <?php require 'blocks/footer.php'; ?>
+</body>
+</html>
